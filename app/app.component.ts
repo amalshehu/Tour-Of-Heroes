101b5925c83a469c175033ac01dbcd218bf53413
Amal Shehu <amalshehu@gmail.com>
2016-10-18 17:46:34 +0530

    create an array of ten heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,19 @@ export class Hero{
   id:number;
   name:string;
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template:`
@@ -17,7 +30,7 @@ export class Hero{
   `
 })
 export class AppComponent {
-
+  heroes = HEROES;
   title = 'Tour of heroes';
   hero: Hero = {
     id: 1,
