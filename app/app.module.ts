328ddc014ddea9f762b798b5cbbaac550832653a
Amal Shehu <amalshehu@gmail.com>
2016-10-20 15:41:03 +0530

    Configure ROutes

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms'
 
-import { AppComponent }   from './app.component';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import { RouterModule }        from '@angular/router';
 
-
+RouterModule.forRoot([
+  {
+    path:'heroes',
+    component: HeroesComponent
+  }
+])
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent],
