0f16f014f69115f5fa7315ca12408e95a45a4b24
Amal Shehu <amalshehu@gmail.com>
2016-10-20 15:03:13 +0530

    Add HeroesComponent to the declarations

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HeroDetailComponent],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
