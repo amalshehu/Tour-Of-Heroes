ae0bf4c8c161e41efc21b50232fb363e7c9d0a27
Amal Shehu <amalshehu@gmail.com>
2016-10-21 15:30:29 +0530

    Add ActivatedRoute

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input } from '@angular/core';
-import { Hero } from './hero';
+import {Component, Input ,OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
 
 @Component({
   selector:'my-hero-detail',
@@ -9,12 +12,24 @@ import { Hero } from './hero';
   <div><label>id: </label>{{hero.id}}</div>
   <div>
     <label>name: </label>
-    <input [(ngModel)] = "hero.name"placeholder = "name">
+    <input [(ngModel)] = "hero.name"placeholder = "name"/>
   </div>
   </div>
   `
 })
-export class HeroDetailComponent{
-  @Input()
-   hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  constructor(
+  private heroService: HeroService,
+  private route: ActivatedRoute,
+  private location: Location
+) {}
+ngOnInit(): void {
+  this.route.params.forEach((params: Params) => {
+    let id = +params['id'];
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  });
+}
+
+  @Input() hero: Hero;
 }
