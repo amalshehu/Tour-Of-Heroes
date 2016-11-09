387b0d2ff6469a9349a058a7d3e1c8634db570b0
amal.shehu@neoito.com <amal.shehu@neoito.com>
2016-11-09 13:07:24 +0530

    Add mock data

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman' },
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'Deadpool' },
+  { id: 14, name: 'Heman' },
+  { id: 15, name: 'Wolverine' },
+  { id: 16, name: 'Magneto' },
+  { id: 17, name: 'Captain America' },
+  { id: 18, name: 'Spiderman' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
